b61ecf223b90af38a4ebb793f3383e89f99fe48b
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2019-04-12 23:36:50 +0530

    Clean up

--- src/components/Home.js
+++ src/components/Home.js
@@ -48,7 +48,7 @@ class App extends Component {
 				.then(res => {
 					console.log(res.data);
 					if (res.data.status && res.data.status === "error") {
-						// Show error to user
+						console.log("Error occurred");
 					} else {
 						this.setState({
 							interestRate: res.data.interestRate,
@@ -69,19 +69,6 @@ class App extends Component {
 		return (
 			<>
 				<NavBar />
-				<div className="jumbotron">
-					<h1 className="display-4">Hello, world!</h1>
-					<p className="lead">
-						This is a simple hero unit, a simple jumbotron-style component for
-						calling extra attention to featured content or information.
-					</p>
-					<hr className="my-4" />
-					<p>
-						It uses utility classes for typography and spacing to space content
-						out within the larger container.
-					</p>
-				</div>
-
 				<div className="container w-100 card">
 					<form>
 						<div className="form-group">
